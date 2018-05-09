96d7b56f30495f8e4b603948a6ebed6a0b30cb72
José Ramón Alemán <jraleman@users.noreply.github.com>
2018-05-09 08:21:32 -0700

    Add seti atom theme

--- atom/theme.sh
+++ atom/theme.sh
@@ -3,7 +3,8 @@
 # List of atom themes.
 list=("city-lights-ui" \
       "city-lights-syntax" \
-      "styri-syntax")
+      "seti-ui" \
+      "seti-syntax")
 
 # Install each theme one by one.
 for thms in ${list[@]}
